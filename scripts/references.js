964577abfb8faf22b37cde0da7fcb05c22630b19
= <=>
2022-09-12 02:14:10 -0300

    Added page selector

---
+++ scripts/references.js
@@ -0,0 +1,9 @@
+const References = {
+    "descriptions": {
+        "permutaciones": [
+            "Denominamos permutación sin repetición a cada una de las formas enque podemos ordenar un conjunto de <font sytle='font-family: MathJax'></font> elementos distintos, siendonunnúmero natural cualquiera.",
+            "Se refiere a las formas en que podemos ordenar un conjunto de n elementos, pero uno o más elementos se pueden repetir.",
+            "Denominamos permutación circular a cada uno de los ordenamientos al rededor de un círculo que podemos realizar con un conjunto de n elementos distintos, siendo n un número natural cualquiera."
+        ]
+    }
+}
---
+++ scripts/setupPageSelector.js
@@ -0,0 +1,26 @@
+const page_selector_container = document.querySelectorAll('.page-selector-container');
+
+page_selector_container.forEach((container) => {
+    const selector = container.querySelector('.page-selector');
+    const selector_items = selector.querySelectorAll('.selector-item');
+    const page_content = container.querySelector(".page-content");
+    const pages = Array.prototype.slice.call(page_content.getElementsByTagName("page"));
+    selector_items.forEach((item) => {
+        const item_title = item.innerHTML;
+
+        item.addEventListener("click", () => {
+            selector_items.forEach((item) => {
+                item.classList.remove("selector-item-selected");
+            })
+            item.classList.add("selector-item-selected");
+            const page_name = item_title.toLowerCase();
+            const page_component = page_content.querySelector(`.page-${page_name}`);
+            
+            pages.forEach((page) => {
+                page.classList.remove("page-visible");
+            });
+
+            page_component.classList.add("page-visible");
+        });
+    });
+});
